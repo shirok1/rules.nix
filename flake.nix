708ca4b4d252c736eb99479b526fa92f4eb2e1b3
Shiroki Satsuki <me@shirok1.dev>
2026-02-14 20:32:57 +0800

    refactor: flake-parts

--- flake.nix
+++ flake.nix
@@ -3,85 +3,64 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    { nixpkgs, ... }:
-    let
-      hashes = builtins.fromJSON (builtins.readFile ./hashes.json);
-
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-
-      mkDat =
-        pkgs':
-        let
-          lib = pkgs'.lib;
-        in
-        lib.mapAttrs (
-          repoName: repoData:
-          lib.mapAttrs (
-            assetName: assetData:
-            pkgs'.fetchurl {
-              name = "${repoName}-${assetName}-${repoData.release}";
-              url = "https://github.com/${repoName}/releases/download/${repoData.release}/${assetName}";
-              hash = assetData.sha256_sri;
-            }
-          ) repoData.assets
-        ) hashes.repos;
-
-      mkCombined =
-        pkgs':
-        {
-          name,
-          geoip,
-          geosite,
-        }:
-        pkgs'.runCommandLocal name { } ''
-          mkdir -p $out/share/v2ray
-          ln -s ${geoip} $out/share/v2ray/geoip.dat
-          ln -s ${geosite} $out/share/v2ray/geosite.dat
-        '';
-    in
-    {
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          lib = pkgs.lib;
-
-          dat = mkDat pkgs;
-
-          combined = lib.mapAttrs (
-            repoName: repoAssets:
-            mkCombined pkgs {
-              name = lib.replaceStrings [ "/" ] [ "-" ] repoName;
-              geoip = repoAssets."geoip.dat";
-              geosite = repoAssets."geosite.dat";
-            }
-          ) dat;
-        in
-        {
-          inherit dat combined;
-
-          default =
-            let
-              firstRepo = builtins.head (builtins.attrNames combined);
-            in
-            combined.${firstRepo};
-
-          tool = combined;
-        }
-      );
-
-      lib = {
-        inherit hashes mkDat mkCombined;
-      };
-    };
+    inputs@{ nixpkgs, flake-parts, ... }:
+    # https://flake.parts/module-arguments.html
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      top@{
+        config,
+        withSystem,
+        moduleWithSystem,
+        ...
+      }:
+      {
+        imports = [
+          # Optional: use external flake logic, e.g.
+          # inputs.foo.flakeModules.default
+        ];
+        flake = {
+          # Put your original flake attributes here.
+          hashes = builtins.fromJSON (builtins.readFile ./hashes.json);
+        };
+        systems =
+          # systems for which you want to build the `perSystem` attributes
+          nixpkgs.lib.systems.flakeExposed;
+        perSystem =
+          {
+            config,
+            pkgs,
+            lib,
+            ...
+          }:
+          let
+            dat = lib.mapAttrs (
+              repoName: repoData:
+              lib.mapAttrs (
+                assetName: assetData:
+                pkgs.fetchurl {
+                  name = "${repoName}-${assetName}-${repoData.release}";
+                  url = "https://github.com/${repoName}/releases/download/${repoData.release}/${assetName}";
+                  hash = assetData.sha256_sri;
+                }
+              ) repoData.assets
+            ) top.config.flake.hashes.repos;
+            combined = lib.mapAttrs (
+              repoName: repoAssets:
+              pkgs.runCommandLocal (lib.replaceStrings [ "/" ] [ "-" ] repoName) { } ''
+                mkdir -p $out/share/v2ray
+                ln -s ${repoAssets."geoip.dat"} $out/share/v2ray/geoip.dat
+                ln -s ${repoAssets."geosite.dat"} $out/share/v2ray/geosite.dat
+              ''
+            ) dat;
+          in
+          {
+            legacyPackages = {
+              inherit dat combined;
+            };
+          };
+      }
+    );
 }
